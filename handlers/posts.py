ad0b37106702669e476738996fbbd18937fb54ca
Kyler Sousley <kylersousley@Kylers-MacBook-Pro.local>
2025-04-01 15:40:59 -0600

    added like and dislike buttons

--- handlers/posts.py
+++ handlers/posts.py
@@ -44,3 +44,37 @@ def delete():
     posts.delete_post(db, post_id)
 
     return flask.redirect(flask.url_for('login.index'))
+
+@blueprint.route('/likepost', methods=['POST'])
+def like():
+    db = helpers.load_db()
+
+    username = flask.request.cookies.get('username')
+    password = flask.request.cookies.get('password')
+
+    user = users.get_user(db, username, password)
+    if not user:
+        flask.flash('You need to be logged in to do that.', 'danger')
+        return flask.redirect(flask.url_for('login.loginscreen'))
+    
+    post_id = flask.request.form.get('post_id')
+    posts.like_post(db, post_id)
+
+    return flask.redirect(flask.url_for('login.index'))
+
+@blueprint.route('/dislikepost', methods=['POST'])
+def dislike():
+    db = helpers.load_db()
+
+    username = flask.request.cookies.get('username')
+    password = flask.request.cookies.get('password')
+
+    user = users.get_user(db, username, password)
+    if not user:
+        flask.flash('You need to be logged in to do that.', 'danger')
+        return flask.redirect(flask.url_for('login.loginscreen'))
+    
+    post_id = flask.request.form.get('post_id')
+    posts.dislike_post(db, post_id)
+
+    return flask.redirect(flask.url_for('login.index'))
